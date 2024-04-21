29a68e4e5ad8e55ae28d58cb93d34f4af6e3660a
Шапошников Игорь <igor.shaposhnikov@urfu.me>
2024-04-21 19:52:12 +0500

    Добавил основу проекта

---
+++ src/main/java/com/thelegance/bookshalf/BookShelfApp.java
@@ -0,0 +1,13 @@
+package com.thelegance.bookshalf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelfApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShelfApp.class, args);
+	}
+
+}
